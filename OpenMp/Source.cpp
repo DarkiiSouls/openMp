cc309bb0baa098f9353c36a34e1a431b3634973a
Dark Souls <darkiisouls@outlook.com>
2018-04-24 13:30:23 +0300

    2 core

--- OpenMp/Source.cpp
+++ OpenMp/Source.cpp
@@ -75,8 +75,39 @@ void selectionSort2(int ilgi[], int n, int bas[])
 
 	double total_clock;
 	total_clock = t2 - t1;
-	cout << endl << "Parallar " << total_clock << " ms" << endl;
+	cout << endl << "Parallar 4 Core" << total_clock << " ms" << endl;
 	
+}
+void selectionSort3(int ilgi[], int n, int bas[])
+{
+	clock_t t1, t2;
+
+	t1 = clock();
+#pragma omp parallel for num_threads(4)
+	for (int x = 0; x<n - 1; x++)
+	{
+		int iMin = x;
+		for (int y = x + 1; y < n; y++)
+		{
+			if (ilgi[y] > ilgi[iMin])
+				iMin = y;
+		}
+		/*int temp = ilgi[x];
+		ilgi[x] = ilgi[iMin];
+		ilgi[iMin] = temp;
+
+
+
+		int temp2 = bas[x];
+		bas[x] = bas[iMin];
+		bas[iMin] = temp2;*/
+	}
+	t2 = clock();
+
+	double total_clock;
+	total_clock = t2 - t1;
+	cout << endl << "Parallar 2 Core" << total_clock << " ms" << endl;
+
 }
 //
 //void selectionSort3(int ilgi[], int n,  int bas[])
@@ -148,7 +179,7 @@ int main()
 	const unsigned int s = 30000;
 	clock_t t1, t2;
 	clock_t t3, t4;
-	
+	clock_t t5, t6;
 	
 	int ilgi[s] ;
 
@@ -181,7 +212,16 @@ int main()
 	t4 = clock();
 	double total_clock1;
 	total_clock1 = (t4 - t3) / 10;
-	cout << endl << "Total time for Parallar " << total_clock1 << " ms" << endl;
+	cout << endl << "Total time for Parallar 4 Core " << total_clock1 << " ms" << endl;
+
+	t5 = clock();
+	for (int i = 0; i < 10; i++) {
+		selectionSort3(ilgi, size(ilgi), bas);
+	}
+	t6 = clock();
+	double total_clock2;
+	total_clock2 = (t6 - t5) / 10;
+	cout << endl << "Total time for Parallar 2 Core " << total_clock2 << " ms" << endl;
 	/*selectionSort3(ilgi, size(ilgi), bas);
 
 	selectionSort4(ilgi, size(ilgi), bas);*/
